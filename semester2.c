7bbcbec3d22a56ca3c8b92c55059c9965f53b83d
Sheela Adhikari <adhikarisheela1000@gmail.com>
2021-04-04 14:44:23 +0545

    second commit

--- semester2.c
+++ semester2.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
+int sum(int x, int y);
+main(){
+    int a, b,s;
+    printf("enter value for a and b:  ");
+    scanf("%d %d", &a, &b);
+    s=sum(a,b);
+    printf("the sum of the given numbers is %d", s);
 
+}
+int sum(int x, int y){
+    int s;
+    s=x+y;
+    return s;
+}
